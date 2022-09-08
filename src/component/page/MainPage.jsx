914814ff381bc7d82b225ab754481b822255056f
KanghoYoo <kangsick98@gmail.com>
2022-09-08 18:33:01 +0900

    Add: 메인 페이지 구현

---
+++ src/component/page/MainPage.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../ui/Button";
+import PostList from "../list/PostList";
+
+const Wrapper = styled.div`
+  width: calc(100% - 32px);
+  padding: 16px;
+  display: flex;
+  flex: column flex-start center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
+
+  & > * {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
+
+function MainPage(props) {
+  const {} = props;
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <Container>
+        <Button
+          title="글 작성하기"
+          onClick={() => {
+            navigate("/post-write");
+          }}
+        />
+
+        <PostList
+          post={data}
+          onClickItem={(item) => {
+            navigate(`/post/${item.id}`);
+          }}
+        />
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default MainPage;
